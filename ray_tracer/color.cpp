a0bb8b914cee8ec13e0fd061b7bc95889486758c
Daniel Ko <kodaniel2015@gmail.com>
2023-08-10 10:10:06 -0400

    bug fix

--- ray_tracer/color.cpp
+++ ray_tracer/color.cpp
@@ -3,13 +3,9 @@
 #include "color.h"
 
 void write_color(std::ostream& out, Color pixel_color) {
-	double r = pixel_color.x();
-	double g = pixel_color.y();
-	double b = pixel_color.z();
-
 	// Writing the translated [0, 255] value of each color component
-	int ir = static_cast<int>(256 * r);
-	int ig = static_cast<int>(256 * g);
-	int ib = static_cast<int>(256 * b);
-	out << ir << " " << ig << " " << ib << " " << "\n";
+	int r = static_cast<int>(255.999 * pixel_color.x());
+	int g = static_cast<int>(255.999 * pixel_color.y());
+	int b = static_cast<int>(255.999 * pixel_color.z());
+	out << r << " " << g << " " << b << " " << "\n";
 }
