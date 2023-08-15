9f0b215984b2b6467628f663bc2cd03f4ca23e59
Daniel Ko <kodaniel2015@gmail.com>
2023-08-14 20:37:14 -0400

    list of hittable objects

---
+++ ray_tracer/hittable_list.cpp
@@ -0,0 +1,30 @@
+#include "hittable_list.h"
+
+HittableList::HittableList(shared_ptr<Hittable> object) {
+	add(object);
+}
+
+void HittableList::clear() {
+	objects.clear();
+}
+
+void HittableList::add(shared_ptr<Hittable> object) {
+	objects.push_back(object);
+}
+
+bool HittableList::hit(const Ray& r, double t_min, double t_max, HitRecord& rec) const {
+	HitRecord temp_record;
+	bool hit_something = false;
+	double closest_so_far = t_max;
+
+	// Moving from the farthest point of the ray to the closest point of the ray and updating the closet_so_far value
+	for (const shared_ptr<Hittable>& obj : objects) {
+		if (obj->hit(r, t_min, closest_so_far, temp_record)) {
+			hit_something = true;
+			closest_so_far = temp_record.t;
+			rec = temp_record;
+		}
+	}
+
+	return hit_something;
+}
---
+++ ray_tracer/hittable_list.h
@@ -0,0 +1,25 @@
+// Stores a list of Hittables
+
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using namespace std;
+
+class HittableList : public Hittable {
+private:
+	vector<shared_ptr<Hittable>> objects;
+public:
+	HittableList(shared_ptr<Hittable> object);
+
+	void clear();
+	void add(shared_ptr<Hittable> object);
+
+	virtual bool hit(const Ray& r, double t_min, double t_max, HitRecord& rec) const override;
+};
+
+#endif
