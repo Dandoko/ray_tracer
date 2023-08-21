52809ba0364109b7742675679c2e965aca36365e
Daniel Ko <kodaniel2015@gmail.com>
2023-08-21 19:58:51 -0400

    material base class

---
+++ ray_tracer/material.h
@@ -0,0 +1,14 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "color.h"
+#include "ray.h"
+
+class HitRecord;
+
+class Material {
+public:
+	virtual bool scatter(const Ray& r, const HitRecord& rec, Color& attenuation, Ray& scattered) const = 0;
+};
+
+#endif
